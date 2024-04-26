84252c3ada1a8f00d5465befec4fe3e31cf01aea
Viktor Kushnir <viktor.kushnir404@gmail.com>
2024-04-26 11:38:26 +0300

    Changed endpoints, edited Boolean isDeleted to boolean, edited validation in updateById method

--- src/main/java/online/book/store/model/Book.java
+++ src/main/java/online/book/store/model/Book.java
@@ -33,5 +33,5 @@ public class Book {
     private String description;
     private String coverImage;
     @Column(nullable = false)
-    private boolean isDeleted = false;
+    private boolean isDeleted;
 }
