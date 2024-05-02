ffcde7a13b4e920f9a57172392f168567c49c2ff
Viktor Kushnir <158613560+vikkushnir@users.noreply.github.com>
2024-05-02 17:46:50 +0300

    Added validation for DTO classes, created CustomGlobalExceptionHandler (#5)
    * Added validation for existing DTO classes, created CustomGlobalExceptionHandler

--- src/main/java/online/book/store/dto/BookRequestDto.java
+++ src/main/java/online/book/store/dto/BookRequestDto.java
@@ -1,13 +1,23 @@
 package online.book.store.dto;
 
+import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
 import java.math.BigDecimal;
 import lombok.Data;
+import org.hibernate.validator.constraints.ISBN;
 
 @Data
 public class BookRequestDto {
+    @NotBlank(message = "field shouldn't be empty")
     private String title;
+    @NotBlank(message = "field shouldn't be empty")
     private String author;
+    @NotBlank
+    @ISBN(message = "should be at least 13 digits")
     private String isbn;
+    @NotNull(message = "field can't be empty")
+    @Min(value = 0, message = "can't be negative")
     private BigDecimal price;
     private String description;
     private String coverImage;
