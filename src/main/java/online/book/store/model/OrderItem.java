ce32c9c806f831dc5df79781f51d9a194d4c1e44
Viktor Kushnir <viktor.kushnir404@gmail.com>
2024-05-22 15:07:27 +0300

    Fixed spaces, added equal&hashcode, toString annotations

--- src/main/java/online/book/store/model/OrderItem.java
+++ src/main/java/online/book/store/model/OrderItem.java
@@ -10,15 +10,19 @@ import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
 import jakarta.persistence.Table;
 import java.math.BigDecimal;
+import lombok.EqualsAndHashCode;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
+import lombok.ToString;
 import org.hibernate.annotations.SQLDelete;
 import org.hibernate.annotations.SQLRestriction;
 
 @Entity
 @Getter
 @Setter
+@ToString
+@EqualsAndHashCode
 @NoArgsConstructor
 @Table(name = "orders_items")
 @SQLDelete(sql = "UPDATE orders_items SET is_deleted = true WHERE id = ?")
@@ -27,9 +31,13 @@ public class OrderItem {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
-    @ManyToOne
+    @ToString.Exclude
+    @EqualsAndHashCode.Exclude
+    @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "order_id", nullable = false)
     private Order order;
+    @ToString.Exclude
+    @EqualsAndHashCode.Exclude
     @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "book_id", nullable = false)
     private Book book;
